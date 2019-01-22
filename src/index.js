44b49ff40a3ff4c15b255f1dbb24fd31cdf9ccf9
Jake Moxon <jake_mox@hotmail.com>
2019-01-22 12:23:30 +0000

    project set up

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from 'react';
+import { render } from 'react-dom';
+import './index.css';
+import './index.html';
+// import Home from './home/home.jsx';
+
+
+class App extends React.Component {
+  render() {
+    return <Home />;
+  }
+}
+
+render(<App />, document.querySelector('#app'));
